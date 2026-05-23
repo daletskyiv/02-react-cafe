aa035ead0662ae2628e3885ac0055b5270c32820
Vladyslav Daletskyi <63199353+daletskyiv@users.noreply.github.com>
2026-05-23 15:13:20 +0300

    add app and vote-options component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,28 @@
+import { useState } from 'react';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import css from './App.module.css';
+import type { Votes, VoteType } from '../../types/votes';
+
 export default function App() {
-    return <div className={css.app}></div>
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  function handleVote(type: VoteType) {
+    setVotes({ ...votes, [type]: votes[type] + 1 });
+  }
+
+  function resetVotes() {
+    setVotes({ ...votes, good: 0, neutral: 0, bad: 0 });
+  }
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-import { createRoot } from 'react-dom/client'
-import 'node_modules/modern-normalize/modern-normalize.css';
-import './index.css'
-import App from './components/App/App.tsx'
+import { createRoot } from 'react-dom/client';
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
+import App from './components/App/App.tsx';
 
-createRoot(document.getElementById("root") as HTMLDivElement).render(
-  <App />,
-);
-
+createRoot(document.getElementById('root') as HTMLDivElement).render(<App />);
